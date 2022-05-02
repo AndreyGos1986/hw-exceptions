93922d693f4973d27f5310d79bb52e856e0133f6
Андрей <gosudarev_a@mail.ru>
2022-05-02 19:20:51 +0300

    Initial commit

---
+++ src/test/java/gosandrey1986/hwexceptions/HwExceptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package gosandrey1986.hwexceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwExceptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
